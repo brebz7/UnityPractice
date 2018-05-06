522832f88f7834edae669f70de84be2ec4ae9ff6
brebz7 <andrei_brebeanu@yahoo.com>
2018-05-07 01:54:24 +0300

    A and D Keys now rotate the cube
    -also moved the position of the sphereSpawnPoint

--- ScriptingPractice/Assets/Scripts/MoveCube.cs
+++ ScriptingPractice/Assets/Scripts/MoveCube.cs
@@ -4,39 +4,33 @@ using UnityEngine;
 
 public class MoveCube : MonoBehaviour {
 
-  //public float moveSpeed = 10f;
-    public float range;
+    public float movementSpeed;
+    public float turnRateSpeed;
 
+    private float movementInputValue;
+    private float turnRateInputValue;
+    private Rigidbody m_rigidbody;
 
-   
     // Use this for initialization
-    void Start () {
-		
+    void Start ()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-        //if (Input.GetKey(KeyCode.W))
-        //    transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-
-        //if (Input.GetKey(KeyCode.S))
-        //    transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
-
-        //if (Input.GetKey(KeyCode.D))
-        //    transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
-
-        //if (Input.GetKey(KeyCode.A))
-        //    transform.Translate(-Vector3.right * moveSpeed * Time.deltaTime);
-
-
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-        float xPos = h * range;
-        float zPos = v * range;
-
-        transform.Translate(Vector3.right * xPos * Time.deltaTime);
-        transform.Translate(Vector3.forward * zPos * Time.deltaTime);
-       
+	void Update ()
+    {
+        movementInputValue = Input.GetAxis("Vertical");
+        turnRateInputValue = Input.GetAxis("Horizontal");   
 	}
+
+    void FixedUpdate ()
+    {
+        Vector3 movement = transform.forward * movementInputValue * 10 * Time.deltaTime;
+        m_rigidbody.MovePosition(m_rigidbody.position + movement);
+
+        float turn = turnRateInputValue * turnRateSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        m_rigidbody.MoveRotation(m_rigidbody.rotation * turnRotation);
+    }
 }
