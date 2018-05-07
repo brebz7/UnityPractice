adaaa2329840c1986a7a8597eb2552824a1fd632
brebz7 <andrei_brebeanu@yahoo.com>
2018-05-07 17:18:24 +0300

    Changing the color of the cube correctly changes the color of the spheres the cube shoots

--- ScriptingPractice/Assets/Scripts/ShootSphere.cs
+++ ScriptingPractice/Assets/Scripts/ShootSphere.cs
@@ -5,20 +5,43 @@ using UnityEngine;
 public class ShootSphere : MonoBehaviour {
 
     public Rigidbody sphere;
+    public Rigidbody sphereRed;
+    public Rigidbody sphereGreen;
+    public Rigidbody sphereBlue;
     public Transform sphereSpawn;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Fire1"))
+		if (Input.GetButtonDown("Fire1") && gameObject.GetComponent<Renderer>().material.color == Color.red)
         {
-            Rigidbody sphereInstance;
-            sphereInstance = Instantiate(sphere, sphereSpawn.position, sphereSpawn.rotation) as Rigidbody;
-            sphereInstance.AddForce(sphereSpawn.forward * 1200);
+            Shoot(sphereRed);
         }
-	}
+
+        else if (Input.GetButtonDown("Fire1") && gameObject.GetComponent<Renderer>().material.color == Color.green)
+        {
+            Shoot(sphereGreen);
+        }
+
+        else if (Input.GetButtonDown("Fire1") && gameObject.GetComponent<Renderer>().material.color == Color.blue)
+        {
+            Shoot(sphereBlue);
+        }
+
+        else if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot(sphere);
+        }
+    }
+
+    void Shoot(Rigidbody sphereType)
+    {
+        Rigidbody sphereInstance;
+        sphereInstance = Instantiate(sphereType, sphereSpawn.position, sphereSpawn.rotation) as Rigidbody;
+        sphereInstance.AddForce(sphereSpawn.forward * 1200);
+    }
 }
