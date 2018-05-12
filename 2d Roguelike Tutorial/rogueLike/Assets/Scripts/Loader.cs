d94592bd5ebc9eec8316eb389cb57426cd6866e6
brebz7 <andrei_brebeanu@yahoo.com>
2018-05-12 17:07:25 +0300

    Added Gameplay

--- 2d Roguelike Tutorial/rogueLike/Assets/Scripts/Loader.cs
+++ 2d Roguelike Tutorial/rogueLike/Assets/Scripts/Loader.cs
@@ -6,15 +6,10 @@ public class Loader : MonoBehaviour {
 
     public GameObject gameManager;
 
-	// Use this for initialization
 	void Awake()
     {
         if (GameManager.instance == null)
             Instantiate(gameManager);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
